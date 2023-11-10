b1e019d1f056bf062b62afe14edb6f89a016c234
amine m. aouini <aminaouini565@gmail.com>
2023-11-11 00:41:23 +0100

    setup download yt audio

---
+++ ytToMp3.py
@@ -0,0 +1,23 @@
+import pytube 
+import os
+
+# get URL
+yt = pytube.YouTube(str(input('Paste desired yt video =>'))) 
+
+#retrieve audio
+audio = yt.streams.filter(only_audio=True).first()
+
+# setup other destination if desired
+print("Enter the destination (leave blank for current directory)") 
+destination = str(input(">> ")) or '.'
+
+# download file
+out_file = audio.download(output_path=destination) 
+
+# change file extention
+base, ext = os.path.splitext(out_file) 
+new_file = base + '.mp3'
+os.rename(out_file, new_file) 
+
+# print successfully downloaded when everything is done correctly.
+print(yt.title + " has been successfully downloaded.")
