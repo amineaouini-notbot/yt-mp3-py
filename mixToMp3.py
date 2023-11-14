2bb21605ac6542dac3f010ee82b95c507c7ffdf6
amineaouini <aminaouini565@gmail.com>
2023-11-14 12:59:53 +0100

    setup selenium driver

--- mixToMp3.py
+++ mixToMp3.py
@@ -1,15 +1,13 @@
-from pytube import Playlist
 import os
+from selenium import webdriver
 import ssl
 ssl._create_default_https_context = ssl._create_stdlib_context
+url = 'https://www.youtube.com/watch?v=QH3Fx41Jpl4&list=RDQH3Fx41Jpl4&start_radio=1'
 
-import requests
-from bs4 import BeautifulSoup
-res = requests.get('https://www.youtube.com/watch?v=QH3Fx41Jpl4&list=RDQH3Fx41Jpl4&start_radio=1')
-soup = BeautifulSoup(res.content, 'html.parser')
-mix = soup.find_all(class_="yt-simple-endpoint style-scope ytd-playlist-panel-video-renderer")
-# mix = Playlist('https://www.youtube.com/watch?v=QH3Fx41Jpl4&list=RDQH3Fx41Jpl4&start_radio=1')
-print(mix)
-# for video in mix:
-#     audio = video.streams.filter(only_audio=True).first()
-#     audio.download(output_path='/dest')
+
+driver = webdriver.Chrome()
+
+driver.get(url)
+
+
+driver.quit()
