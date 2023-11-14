5000a154e9ad5c0aa6417cb6065918f17811a2d7
amineaouini <aminaouini565@gmail.com>
2023-11-14 15:54:56 +0100

    crawl mix's video links

--- mixToMp3.py
+++ mixToMp3.py
@@ -1,13 +1,19 @@
 import os
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait 
 import ssl
 ssl._create_default_https_context = ssl._create_stdlib_context
 url = 'https://www.youtube.com/watch?v=QH3Fx41Jpl4&list=RDQH3Fx41Jpl4&start_radio=1'
 
 
 driver = webdriver.Chrome()
-
 driver.get(url)
 
-
-driver.quit()
+elements = driver.find_elements(By.CSS_SELECTOR, "a.yt-simple-endpoint.style-scope.ytd-playlist-panel-video-renderer")
+# elements = WebDriverWait(driver,10).until(lambda x: x.find_elements(By.CSS_SELECTOR, "a.yt-simple-endpoint.style-scope.ytd-playlist-panel-video-renderer"))
+# elements = elements.find_elements('.yt-simple-endpoint .style-scope .ytd-playlist-panel-video-renderer')
+print(elements)
+for elemnt in elements:
+    print(elemnt.get_attribute('href'))
+# driver.quit()
